6aea3d72b406221cbd314777bd853ba9f0c3c4ca
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-01-21 06:57:18 -0600

    stuff

--- install_cmd.bat
+++ install_cmd.bat
@@ -3,11 +3,27 @@
 echo %UserProfile%
 
 SET Source=D:\development\cwp_app
-echo directory to install Media 
-set /p var=Enter destination folder for /MediaCreator (Default %USERPROFILE%/Desktop) Use "'s if there are spaces" || SET var=%USERPROFILE%\Desktop
-SET Target=%var%\MediaCreator
-if NOT EXIST %Target% (mkdir %Target%)
+ echo directory to install Media 
+ set /p var=Enter destination folder for /MediaCreator (Default %USERPROFILE%/Desktop) Use "'s if there are spaces" || SET var=%USERPROFILE%\Desktop
+ SET Target=%var%\MediaCreator
+ if NOT EXIST %Target% (mkdir %Target%)
 
 
 
-xcopy %Source% %Target% /E /Y /EXCLUDE:%Source%\exclude.txt
+REM echo xcopy %Source% %Target% /F /E /H /Y /EXCLUDE:%Source%\exclude.txt
+
+
+SetLocal EnableDelayedExpansion
+
+Set "exFList=%Source%\excludeFiles.txt"
+Set "exDList=%Source%\excludeDir.txt"
+Set "xFiles="
+Set "xDirs="
+
+For /F UseBackQDelims^=^ EOL^= %%A In ("%exDList%"
+) Do If Not Defined xDirs (Set xDirs="%%~A") Else Set xDirs=!xDirs! "%%~A"
+
+For /F UseBackQDelims^=^ EOL^= %%A In ("%exFList%"
+) Do If Not Defined xFiles (Set xFiles="%%~A") Else Set xFiles=!xFiles! "%%~A"
+
+robocopy %Source%  %Target% /v /mir /XF %xFiles% /XD %xDirs% /log:log.txt /np >nul 2>&1
