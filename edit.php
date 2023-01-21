4db836fe1aca3cb1a096284f04f68f7405935f21
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-01-21 13:53:05 -0600

    f

--- edit.php
+++ edit.php
@@ -6,7 +6,7 @@ $target_dir = $argv[2];
 $php_file = $source_dir . "/.php/php.ini";
 $target_php_ini = $target_dir . "/.php/php.ini";
 
-
+/*
 if (file_exists($target_php_ini)) {
     unlink($target_php_ini);
 }
@@ -16,3 +16,8 @@ $file_contents = file_get_contents($php_file);
 $file_contents = str_replace("zend_extension", ";zend_extension", $file_contents);
 $file_contents = str_replace("xdebug", ";xdebug", $file_contents);
 file_put_contents($target_php_ini, $file_contents);
+*/
+echo getcwd() . "\n";
+chdir($target_dir ."/.public/www/library");
+echo getcwd() . "\n";
+echo `dir`;
