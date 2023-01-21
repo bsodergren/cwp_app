44e445750d41579c1ef57106a257298442395a4c
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-01-21 14:35:58 -0600

    sources

--- install_cmd.bat
+++ install_cmd.bat
@@ -2,7 +2,7 @@
 
 echo %UserProfile%
 
-SET Source=D:\development\cwp_app
+SET Source=%cd%
 echo directory to install Media 
 set /p var=Enter destination folder for /MediaCreator (Default %USERPROFILE%/Desktop) Use "'s if there are spaces" || SET var=%USERPROFILE%\Desktop
 SET Target=%var%\MediaCreator
@@ -15,12 +15,15 @@ Set "exDList=%Source%\excludeDir.txt"
 Set "xFiles="
 Set "xDirs="
 
-For /F UseBackQDelims^=^ EOL^= %%A In ("%exDList%"
-) Do If Not Defined xDirs (Set xDirs="%%~A") Else Set xDirs=!xDirs! "%%~A"
+For /F UseBackQDelims^=^ EOL^= %%D In ("%exDList%"
+) Do If Not Defined xDirs (Set xDirs="%Source%\%%~D") Else Set xDirs=!xDirs! "%Source%\%%~D"
 
-For /F UseBackQDelims^=^ EOL^= %%A In ("%exFList%"
-) Do If Not Defined xFiles (Set xFiles="%%~A") Else Set xFiles=!xFiles! "%%~A"
+For /F UseBackQDelims^=^ EOL^= %%F In ("%exFList%"
+) Do If Not Defined xFiles (Set xFiles="%Source%\%%~F") Else Set xFiles=!xFiles! "%Source%\%%~F"
 
-robocopy %Source%  %Target% /v /mir /XF %xFiles% /XD %xDirs% /np >nul 2>&1
-php -n -f edit.php "%Source%" "%Target%"
+REM echo robocopy %Source%  %Target% /v /mir /XF %xFiles% /XD %xDirs%
+robocopy %Source%  %Target% /V /mir /XF %xFiles% /XD %xDirs% 
+REM /np >nul 2>&1
+
+if EXIST "%Source%/edit.php" (%Source%/.php/php -n -f %Source%/edit.php "%Source%" "%Target%")
 
