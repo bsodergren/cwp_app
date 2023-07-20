e4203ad73326811180a309e74734c0c3a967e5ce
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-07-20 13:11:52 -0500

    ggg

--- build.bat
+++ build.bat
@@ -1,36 +1,50 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-if "%~1" equ "" echo Error: Insufficient arguments>&2&exit /b 1
-
-echo %1
-set build_bin=D:\development\cwp_app\.bin\builder.exe
-set patch_bin=D:\development\cwp_app\.bin\patcher.exe
-
-set web_dir=D:\development\cwp_app\public
-
-set version_file=D:\development\cwp_app\public\www\updater\version.txt
-set current_file=D:\development\cwp_app\public\www\updater\current.txt
 set build_dir=D:\development\cwp_app
 set old_dir=D:\development\media
-set build_options=D:\development\cwp_app\.bin\sample.cfg
-set zip_dir=D:\development\cwp_app\public\www\updater\versions
-set zip_file=update_%1.zip
+set bin_dir=%build_dir%\bin
+set web_dir=%build_dir%\public
+set updateDir=%build_dir%\public\www\updater
+set zip_dir=%updateDir%\versions
+
+
+set build_bin=%bin_dir%\builder.exe
+set patch_bin=%bin_dir%\patcher.exe
+set build_options=%bin_dir%\sample.cfg
+
+set version_file=%updateDir%\version.txt
+set current_file=%updateDir%\current.txt
+
+
+FOR /F %%i IN ( %current_file%) DO set string=%%i
+
+for /F "tokens=1,2,3 delims=. " %%a in ("%string%") do (
+   set number=%%a%%b%%c
+   set /a number+=1
+)
+
+
+set versionNumber=%number:~0,1%.%number:~1,1%.%number:~2,1%
+
+
+set zip_file=update_%versionNumber%.zip
 set patchzip=%zip_dir%\%zip_file%
 
 if exist %patchzip%  echo Error: Already did Update>&2&exit /b 1
 
 if not exist %zip_dir% mkdir %zip_dir%
+echo Updating to version %versionNumber% 
 
-echo %1 >> %version_file%
-echo %1 > %current_file%
+echo %versionNumber% >> %version_file%
+echo %versionNumber% > %current_file%
 
 %build_bin% -N %build_dir% -O %old_dir% -C %build_options% -P %patchzip%
 
 chdir /d %web_dir%
 
 git add %patchzip% %version_file% %current_file%
-git commit -m "Updates to v %1"
+git commit -m "Updates to v %versionNumber%"
 git push
 
 chdir /d %old_dir%
--- patch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set patchbin=%cd%\.bin\patcher.exe
-set olddir=%cd%
-
-::set patchzip=%cd%\public\www\updater\Latest.zip
-set patchzip=D:\development\cwp_app\public\www\updater\Latest.zip
-
-%patchbin% -O %olddir% -P %patchzip%
