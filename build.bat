74ba747ad57d6887658febfeca20146f567bf1b6
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-08-02 07:14:16 -0500

    kk

--- build.bat
+++ build.bat
@@ -5,17 +5,15 @@ set build_dir=D:\development\cwp_app
 set old_dir=D:\development\media
 set bin_dir=%build_dir%\bin
 set web_dir=%build_dir%\public
-set updateDir=%build_dir%\public\www\updater
-set zip_dir=%updateDir%\versions
+set updateDir=%build_dir%\public\AppUpdates
 
 
 set build_bin=%bin_dir%\builder.exe
 set patch_bin=%bin_dir%\patcher.exe
 set build_options=%bin_dir%\sample.cfg
 
-set version_file=%updateDir%\version.txt
-set current_file=%updateDir%\current.txt
-
+set current_file=%web_dir%\current.txt
+set json_file=%updateDir%\update.json
 
 FOR /F %%i IN ( %current_file%) DO set string=%%i
 
@@ -27,25 +25,19 @@ for /F "tokens=1,2,3 delims=. " %%a in ("%string%") do (
 
 set versionNumber=%number:~0,1%.%number:~1,1%.%number:~2,1%
 
-
-set zip_file=update_%versionNumber%.zip
-set patchzip=%zip_dir%\%zip_file%
-
-if exist %patchzip%  echo Error: Already did Update>&2&exit /b 1
-
-if not exist %zip_dir% mkdir %zip_dir%
-echo Updating to version %versionNumber% 
-
-echo %versionNumber% >> %version_file%
 echo %versionNumber% > %current_file%
 
-%build_bin% -N %build_dir% -O %old_dir% -C %build_options% -P %patchzip%
+set zip_file=%versionNumber%.zip
+set patchzip=%updateDir%\%zip_file%
 
-chdir /d %web_dir%
 
-git add %patchzip% %version_file% %current_file%
-git commit -m "Updates to v %versionNumber%"
-git push
+if EXIST "%build_dir%/update.php" (%build_dir%/php/php  -f %build_dir%/update.php "%web_dir%" "%versionNumber%")
 
-chdir /d %old_dir%
-%patch_bin% -O %old_dir% -P %patchzip%
+ chdir /d %web_dir%
+
+  git add  %current_file% %json_file%  %patchzip%
+  git commit -m "Updates to v %versionNumber%"
+  git push
+
+REM chdir /d %old_dir%
+REM %patch_bin% -O %old_dir% -P %patchzip%
---
+++ update.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * CWP Media tool
+ */
+
+ 
+$version = $argv[2];
+
+$drive_letter = substr($argv[1], 0, 2);
+$target_dir = $drive_letter.str_replace($drive_letter, '', $argv[1]);
+$web_dir = $target_dir; // .\DIRECTORY_SEPARATOR.'public';
+$update_dir = $target_dir.\DIRECTORY_SEPARATOR.'AppUpdates';
+
+$update_zips = $update_dir;
+
+$update_json = $update_zips.\DIRECTORY_SEPARATOR.'update.json';
+$fileList = $update_zips.\DIRECTORY_SEPARATOR.'previous.txt';
+$deleteList = $web_dir.\DIRECTORY_SEPARATOR.'delete.txt';
+
+
+
+$skipdirs = [
+   'public\\AppUpdates',
+   //'www\\updater\\updater_versions',
+   //'www\\updater\\download',
+   'public\\database',
+   'public\\cache',
+   'www\\files',
+   '.vscode',
+   '.git',
+];
+
+$skipfiles =
+[
+    'update.log',
+'config.ini',
+'test_navlinks.php',
+'.gitignore',
+'.editorconfig',
+'.php-cs-fixer.php',
+'php_errors.log',
+];
+
+function listAllFiles($dir)
+{
+    $array = array_diff(scandir($dir), ['.', '..']);
+
+    foreach ($array as &$item) {
+        $item = $dir.\DIRECTORY_SEPARATOR.$item;
+    }
+    unset($item);
+    foreach ($array as $item) {
+        if (is_dir($item)) {
+            $array = array_merge($array, listAllFiles($item));
+        }
+    }
+
+    return $array;
+}
+
+
+$files_array = listAllFiles($target_dir);
+
+foreach ($files_array as $file) {
+    $exists = false;
+    if (!str_contains($file, \DIRECTORY_SEPARATOR.'vendor')) {
+        foreach ($skipdirs as $skip) {
+            if (str_contains($file, $skip)) {
+                $exists = true;
+                continue;
+            }
+        }
+        foreach ($skipfiles as $skip) {
+            if (str_contains($file, $skip)) {
+                $exists = true;
+                continue;
+            }
+        }
+    }
+    if (false == $exists) {
+        $newFiles[] = $file;
+    }
+}
+
+$currentList = file_get_contents($fileList);
+$newList_Array = explode("\n", $currentList);
+
+$deletedArray = array_diff($newList_Array, $newFiles);
+
+if (count($deletedArray) > 0)
+{
+    $deleted_str = implode("\n", $deletedArray);
+    $deleted_str = str_replace($web_dir.\DIRECTORY_SEPARATOR, '', $deleted_str);
+
+    file_put_contents($deleteList, $deleted_str);
+} else {
+    unlink($deleteList);
+}
+
+$fileList_str = implode("\n", $newFiles);
+file_put_contents($fileList, $fileList_str);
+
+$zip_file = $update_zips.\DIRECTORY_SEPARATOR.$version.'.zip';
+$rootPath = $web_dir;
+$zipPath = str_replace(basename($zip_file), '', $zip_file);
+
+
+$zip = new ZipArchive();
+$zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+foreach ($newFiles as $file) {
+    if (!is_dir($file)) {
+        $filePath = realpath($file);
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+        $zip->addFile($filePath, $relativePath);
+    }
+}
+
+if(file_exists($deleteList))
+{
+    $filePath = realpath($deleteList);
+    $relativePath = substr($filePath, strlen($rootPath) + 1);
+    $zip->addFile($filePath, $relativePath);
+
+}
+
+// Zip archive will be created only after closing object
+$zip->close();
+
+
+$update_files = listAllFiles($update_zips);
+
+$url = 'https://raw.githubusercontent.com/bsodergren/cwp_www/main/AppUpdates/';
+$iniString = '';
+
+foreach ($update_files as $zipFile) {
+    if ($zipFile == $update_json || 
+    $zipFile == $fileList ||
+    $zipFile == $deleteList ) {
+        continue;
+    }
+    
+    $zipversion = basename($zipFile, '.zip');
+    $zipurl = $url.basename($zipFile);
+    $jsonArray[] = "\t".'"'.$zipversion.'":"'.$zipurl.'"';
+    $iniString .= '['.$zipversion."]\n".'url = '.$zipurl."\n";
+}
+
+$json_string = "{ \n";
+$json_string .= implode(",\n", $jsonArray);
+$json_string .= "\n}";
+
+file_put_contents($update_json, $json_string);
+
+//file_put_contents($update_ini, $iniString);
