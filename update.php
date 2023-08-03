19a00416949d569c3c9f3e8673332d52d929ff90
bjorn sodergren <bjorn.sodergren@gmail.com>
2023-08-03 05:51:51 -0500

    gg

--- update.php
+++ update.php
@@ -3,7 +3,6 @@
  * CWP Media tool
  */
 
- 
 $version = $argv[2];
 
 $drive_letter = substr($argv[1], 0, 2);
@@ -17,12 +16,11 @@ $update_json = $update_zips.\DIRECTORY_SEPARATOR.'update.json';
 $fileList = $update_zips.\DIRECTORY_SEPARATOR.'previous.txt';
 $deleteList = $web_dir.\DIRECTORY_SEPARATOR.'delete.txt';
 
-
-
 $skipdirs = [
    'public\\AppUpdates',
-   //'www\\updater\\updater_versions',
-   //'www\\updater\\download',
+   'public\\vendor',
+   // 'www\\updater\\updater_versions',
+   // 'www\\updater\\download',
    'public\\database',
    'public\\cache',
    'www\\files',
@@ -33,12 +31,12 @@ $skipdirs = [
 $skipfiles =
 [
     'update.log',
-'config.ini',
-'test_navlinks.php',
-'.gitignore',
-'.editorconfig',
-'.php-cs-fixer.php',
-'php_errors.log',
+    'config.ini',
+    'test_navlinks.php',
+    '.gitignore',
+    '.editorconfig',
+    '.php-cs-fixer.php',
+    'php_errors.log',
 ];
 
 function listAllFiles($dir)
@@ -58,25 +56,23 @@ function listAllFiles($dir)
     return $array;
 }
 
-
 $files_array = listAllFiles($target_dir);
 
 foreach ($files_array as $file) {
     $exists = false;
-    if (!str_contains($file, \DIRECTORY_SEPARATOR.'vendor')) {
-        foreach ($skipdirs as $skip) {
-            if (str_contains($file, $skip)) {
-                $exists = true;
-                continue;
-            }
-        }
-        foreach ($skipfiles as $skip) {
-            if (str_contains($file, $skip)) {
-                $exists = true;
-                continue;
-            }
+    foreach ($skipdirs as $skip) {
+        if (str_contains($file, $skip)) {
+            $exists = true;
+            continue;
         }
     }
+    foreach ($skipfiles as $skip) {
+        if (str_contains($file, $skip)) {
+            $exists = true;
+            continue;
+        }
+    }
+
     if (false == $exists) {
         $newFiles[] = $file;
     }
@@ -87,12 +83,11 @@ $newList_Array = explode("\n", $currentList);
 
 $deletedArray = array_diff($newList_Array, $newFiles);
 
-if (count($deletedArray) > 0)
-{
+if (count($deletedArray) > 0) {
     $deleted_str = implode("\n", $deletedArray);
     $deleted_str = str_replace($web_dir.\DIRECTORY_SEPARATOR, '', $deleted_str);
 
-    file_put_contents($deleteList, $deleted_str);
+    // file_put_contents($deleteList, $deleted_str);
 } else {
     unlink($deleteList);
 }
@@ -103,7 +98,6 @@ file_put_contents($fileList, $fileList_str);
 $zip_file = $update_zips.\DIRECTORY_SEPARATOR.$version.'.zip';
 $rootPath = $web_dir;
 $zipPath = str_replace(basename($zip_file), '', $zip_file);
-
 
 $zip = new ZipArchive();
 $zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
@@ -116,17 +110,14 @@ foreach ($newFiles as $file) {
     }
 }
 
-if(file_exists($deleteList))
-{
+if (file_exists($deleteList)) {
     $filePath = realpath($deleteList);
     $relativePath = substr($filePath, strlen($rootPath) + 1);
     $zip->addFile($filePath, $relativePath);
-
 }
 
 // Zip archive will be created only after closing object
 $zip->close();
-
 
 $update_files = listAllFiles($update_zips);
 
@@ -134,12 +125,12 @@ $url = 'https://raw.githubusercontent.com/bsodergren/cwp_www/main/AppUpdates/';
 $iniString = '';
 
 foreach ($update_files as $zipFile) {
-    if ($zipFile == $update_json || 
-    $zipFile == $fileList ||
-    $zipFile == $deleteList ) {
+    if ($zipFile == $update_json
+    || $zipFile == $fileList
+    || $zipFile == $deleteList) {
         continue;
     }
-    
+
     $zipversion = basename($zipFile, '.zip');
     $zipurl = $url.basename($zipFile);
     $jsonArray[] = "\t".'"'.$zipversion.'":"'.$zipurl.'"';
@@ -152,4 +143,4 @@ $json_string .= "\n}";
 
 file_put_contents($update_json, $json_string);
 
-//file_put_contents($update_ini, $iniString);
+// file_put_contents($update_ini, $iniString);
