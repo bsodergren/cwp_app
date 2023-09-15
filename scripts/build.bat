f516532429e6ae921491663dcf62c013d0dab909
Bjorn Sodergren <bjorn.sodergren@gmail.com[filter lfs]>
2023-09-15 04:58:30 -0500

    ll

--- scripts/build.bat
+++ scripts/build.bat
@@ -17,14 +17,25 @@ set versionNumber=%number:~0,1%.%number:~1,1%.%number:~2,1%
 
 echo %versionNumber% > %current_file%
 
-set zip_file=%versionNumber%.zip
-set patchzip=%app_updatesr%\%zip_file%
+@REM set zip_file=%versionNumber%.zip
+@REM set patchzip=%app_updatesr%\%zip_file%
 
 
-if EXIST "%update_script%" (%php_exec%  -f %update_script% "%update_dir%" "%versionNumber%")
+@REM if EXIST "%update_script%" (%php_exec%  -f %update_script% "%update_dir%" "%versionNumber%")
 
- chdir /d %web_dir%
+chdir /d %web_dir%
 
-  git add  %current_file% %json_file%  %patchzip%
-  git commit -m "Updates to v %versionNumber%"
-  git push
+git add  .
+git commit -m "Updates to v %versionNumber%"
+git push
+git checkout -b %versionNumber% develop
+git push --set-upstream origin %versionNumber%
+git checkout main
+git merge %versionNumber%
+git tag -a %versionNumber% -m "%versionNumber% public release" main
+git push --tags
+git push
+git checkout develop
+git merge %versionNumber%
+git push
+git branch -d %versionNumber%
